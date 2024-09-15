e655871b6f793e04e52aaa7d9b0ad25479764831
Danny <danny@lilytronics.nl>
2024-09-15 15:23:07 +0200

    update app name

--- application/app_info.py
+++ application/app_info.py
@@ -5,5 +5,5 @@ Application information
 
 class AppInfo:
 
-    NAME = "Lily System\u2122 control"      # \u2122: trademark symbol
+    NAME = "Lily System\u2122 Control"      # \u2122: trademark symbol
     VERSION = "0.1"
