4246d41f212c12631c42405c21c91e947c388e23
Danny <danny@lilytronics.nl>
2024-09-17 14:08:22 +0200

    add string representation of packet

--- application/models/data_packet.py
+++ application/models/data_packet.py
@@ -3,8 +3,10 @@ Data packet class.
 Convert packet to send into data top send and convert received data into a packet.
 
 Data packet format: <STX><DSN><SSN><PID><DATA><CRC><ETX>
-Number of bytes   :   1    1    1    2    35    1    1
-Data max 35 bytes.
+Number of bytes   :   1    1    1    2     n    1    1
+Data 1 ... 35 bytes.
+Minimum packet size: 8
+Maximum packet size: 42
 Bytes stuffing using DLE.
 """
 
@@ -16,16 +18,38 @@ class DataPacket:
     STX = 0x02
     ETX = 0x04
     DLE = 0x20
+    MIN_PACKET_SIZE = 8
 
     def __init__(self):
         self.dsn = 0
         self.ssn = 0
         self.pid = 0
-        self.command = 0
+        self.data = []
 
-    def _apply_byte_stuffing(self, data_bytes):
+    def __str__(self):
+        output = "Properties:\n"
+        output += f"DSN  : {self.dsn}\n"
+        output += f"SSN  : {self.ssn}\n"
+        output += f"PID  : {self.pid}\n"
+        output += f"Data : {self._represent(self.data)}\n"
+        output += f"Bytes: {self.get_data()}"
+        return output
+
+    @staticmethod
+    def _represent(data):
+        hex_output = []
+        ascii_output = ""
+        for byte in data:
+            hex_output.append(f"0x{byte:02X}")
+            if 31 < byte < 127:
+                ascii_output += chr(byte)
+            else:
+                ascii_output += "."
+        return f"{" ".join(hex_output)} - {ascii_output}"
+
+    def _apply_byte_stuffing(self, data):
         stuffed_data = []
-        for byte in data_bytes:
+        for byte in data:
             if byte in [ self.STX, self.ETX, self.DLE]:
                 stuffed_data.append(self.DLE)
                 stuffed_data.append(~byte & 0xFF)
@@ -33,26 +57,52 @@ class DataPacket:
                 stuffed_data.append(byte)
         return stuffed_data
 
+    def _remove_byte_stuffing(self, data):
+        unstuffed_data = []
+        stuffed_byte = False
+        for byte in data:
+            if byte == self.DLE:
+                stuffed_byte = True
+                continue
+            if stuffed_byte:
+                unstuffed_data.append(~byte & 0xFF)
+                stuffed_byte = False
+            else:
+                unstuffed_data.append(byte)
+        return unstuffed_data
+
+    def from_data(self, data):
+        if data[0] == self.STX and data[-1] == self.ETX and len(data) >= self.MIN_PACKET_SIZE:
+            data = self._remove_byte_stuffing(data[1:-1])
+            self.dsn = data[0]
+            self.ssn = data[1]
+            self.pid = data[2] * 256 + data[3]
+            self.data = data[4:-1]
+
     def get_data(self):
         data = [
             self.dsn,
             self.ssn,
             (self.pid >> 8) & 0xFF,
             self.pid & 0xFF,
-            self.command
         ]
+        data.extend(self.data)
         crc = calculate_crc(data)
         data.append(crc)
         data = self._apply_byte_stuffing(data)
-        return bytes([self.STX]) + bytes(data) + bytes([self.ETX])
+        data.insert(0, self.STX)
+        data.append(self.ETX)
+        return bytes(data)
 
 
 if __name__ == "__main__":
 
     packet = DataPacket()
-    packet.dsn = 4
-    packet.ssn = 2
-    packet.pid = 0x0103
-    packet.command = 0x20
+    packet.dsn = 1
+    packet.ssn = 3
+    packet.pid = 0x0506
+    packet.data = [0x07, 0x11, 0x20]
+    print(packet)
 
-    print(packet.get_data())
+    packet.from_data(b"\x02\x01\x03\x05\x06\x07\x11 \xdf\x89\x04")
+    print(packet)
